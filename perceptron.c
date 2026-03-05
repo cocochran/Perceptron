3488fbece8e516155f140912560cc5c842fabf5f
Connor M. Cochran <connor.cochran@outlook.com>
2026-03-04 23:48:04 -0500

    refactored and finished perceptron.c

--- perceptron.c
+++ perceptron.c
@@ -1,76 +1,109 @@
 #include <stdio.h>
-
-/*
-    Perceptron computes y=sign(w*x+b) where
-        - w is a float weight vector
-        - b is a float bias
-        - sign(z) = 1 if z>=0, otherwise -1
-
-    The variable y signifies the activation (or non-activation) of the perceptron.
-*/
-
-float dot_product(float x[][2], float *w, int n);
-int predict(float x[][2], float *w, int n);
-void train(float x[][2], float *w, float *b, int *y, float *eta, int n);
-void perceptron();
-
+#include <stdlib.h>
 
 struct perceptron {
-    float w[2];
+    float *w;
     float b;
     float eta;
 };
 
+float dot_product(float x[], float *w, int n);
+int predict(float x[], struct perceptron *p, int n);
+void train(float x[][2], int *y, struct perceptron *p, int n, int samples, int dimensions);
+void print_epoch(int epoch, int errors, int dimensions, FILE *log, struct perceptron *p);
+void print_perceptron(int dimensions, FILE *log, struct perceptron *p);
+
+
 int main(){
+    int dimensions = 2;
     float x[4][2] = {
         {1.0, 1.0},
-        {2.0, 2.0},
+        {2.0, 1.0},
         {-1.0, -1.0},
         {-2.0, -1.0}
     };
 
     int y[4] = {1, 1, -1, -1};
 
-    struct perceptron p;
+    //tried this with other sample sizes, so had to do it dynamically 
+    struct perceptron p = {calloc(dimensions, sizeof(float)), 0.0, 0.1};
 
-    p.w[0] = 0.0;
-    p.w[1] = 0.0;
-    p.b = 0.0;
-    p.eta = 0.1;
-
-    train(x, p.w, &p.b, y, &p.eta, 50);
-    printf("W[0]=%f, W[1]=%f, b=%f, eta=%f", p.w[0], p.w[1], p.b, p.eta);
+    train(x, y, &p, 50, 4, 2);
+    free(p.w);
     return 0;
 }
 
 /* Returns the dot product of a two floating-point vectors. */
-float dot_product(float x[][2], float *w, int n){
+float dot_product(float x[], float *w, int dimensions){
     float sum = 0.0;
-    for (int i = 0; i < n; i++){
-        sum += (**(x + i)) * (*(w + i));
+    for (int i = 0; i < dimensions; i++){
+        sum += (*(x + i)) * (*(w + i));
     }
     return sum;
 }
 
-int predict(float x[][2], float *w, int n){
-    return dot_product(x, w, n) >= 0.0 ? 1 : -1;
+/* Predicts the value 1 or -1 by using the dot product and adding the perceptron bias.*/
+int predict(float x[], struct perceptron *p, int n){
+    return dot_product(x, p->w, n) + p->b >= 0.0 ? 1 : -1;
 }
 
-void train(float x[][2], float *w, float *b, int *y, float *eta, int n){
-    fopen("/output/traing-log.txt", "w");
-    for (int k = 0; k < n; k++){
-        for (int i = 0; i < 4; i++){
-            for (int j = 0; j < 2; j++){
-		if (predict(x, w, n) == y[j]) return;
-                w[j] = w[j] + *eta * y[i] * x[i][j];
+/* 
+Trains a perceptron using n epochs, utilizing the arrays x and y as sample data.
+*/
+void train(float x[][2], int *y, struct perceptron *p, int n, int samples, int dimensions) {
+    FILE *log = fopen("training-log.txt", "w");
 
-		
+    for (int k = 0; k < n; k++) {
+        int all_correct = 1;
+        int errors = 0;
+        for (int i = 0; i < samples; i++) {
+            if (predict(x[i], p, dimensions) != y[i]) {
+                all_correct = 0;
+                errors++;
+                for (int j = 0; j < dimensions; j++)
+                    p->w[j] = p->w[j] + p->eta * y[i] * x[i][j];
+                p->b = p->b + p->eta * y[i];
             }
-            *b = *b + *eta * y[i];
-            printf("Epoch %d: w[0]=%f, w[1]=%f, b=%f, eta=%f\n", k, w[0], w[1], *b, *eta);
 
-        }
+        }   
+        print_epoch(k + 1, errors, dimensions, log, p);
+        if (all_correct == 1) break;
     }
+    print_perceptron(dimensions, log, p);
+    fclose(log);
 }
+
+/* Helper function to print out the data of an epoch. */
+void print_epoch(int epoch, int errors, int dimensions, FILE *log, struct perceptron *p){
+    printf("Epoch %d: errors = %d, w = [", epoch, errors);
+    fprintf(log, "Epoch %d: errors = %d, w = [", epoch, errors);
+
+    for (int i = 0; i < dimensions - 1; i++){
+        printf("%.2f, ", p->w[i]);
+        fprintf(log, "%.2f, ", p->w[i]);
+
+    }    
+    printf("%.2f], ", p->w[dimensions-1]);
+    printf("b = %.2f\n", p->b);
+    fprintf(log, "%.2f], ", p->w[dimensions-1]);
+    fprintf(log, "b = %.2f\n", p->b);
+}
+
+/* Helper function to print out the final data of a trained perceptron. */
+void print_perceptron(int dimensions, FILE *log, struct perceptron *p){
+    printf("Final: w = [");
+    fprintf(log, "Final: w =[");
+
+    for (int i = 0; i < dimensions-1; i++){
+        printf("%.2f, ", p->w[i]);
+        fprintf(log, "%.2f, ", p->w[i]);
+
+    }    
+    printf("%.2f], ", p->w[dimensions-1]);
+    printf("b = %.2f\n", p->b);
+    fprintf(log, "%.2f], ", p->w[dimensions-1]);
+    fprintf(log, "b = %.2f\n", p->b);
+}
+
 
 
